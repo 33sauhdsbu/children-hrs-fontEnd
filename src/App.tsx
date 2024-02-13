9a7d702a7722f4e053204b300f93dd51b46fab8b
Gaohanxing <1667246767.@qq.com>
2024-02-13 15:53:05 +0800

    feat: 配置 Login 登录的文件夹

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import { Button } from 'antd';
+import Login from './Pages/login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => {
-          // axios.get('http://localhost:8000').then((res) => {
-          //   console.info(res);
-          // })
-        }}>Login</button>
+        <Login></Login>
       </header>
     </div>
   );
---
+++ src/Pages/login/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+
+const Login = () => {
+    return <>
+        <Button onClick={() => { alert(1); }}>
+            Login
+        </Button>
+
+    </>;
+}
+
+export default Login;
