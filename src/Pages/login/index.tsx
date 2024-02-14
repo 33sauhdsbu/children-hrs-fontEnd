59c54002b68f1de988278d646f247365f84c2104
Gaohanxing <1667246767.@qq.com>
2024-02-14 11:14:47 +0800

    feat: 搭建 login 登录页面

--- src/Pages/login/index.tsx
+++ src/Pages/login/index.tsx
@@ -1,12 +1,50 @@
-import { Button } from 'antd'
-import React from 'react'
+import { Button, Card, Form, Input } from 'antd'
+import React, { useState } from 'react'
+import './index.less'
 
+const { Item } = Form
 const Login: React.FC = () => {
-  return <div>
-        <Button onClick={() => { alert(1) }}>
-            Login
+  const [isSign, setIsSign] = useState<boolean>(false)
+  return isSign
+    ? <Card style={{ width: 500, height: 350 }}>
+    <strong style={{ fontSize: 20 }}>
+     儿童医院预约挂号系统
+    </strong>
+    <Form style={{ marginTop: 50, marginBottom: 50 }} onFinish={() => {}}>
+        <Item label='User' style={{ height: 30 }}>
+           <Input></Input>
+        </Item>
+        <Item label='Phone' style={{ height: 30 }}>
+           <Input></Input>
+        </Item>
+        <Item label='Password'>
+            <Input></Input>
+        </Item>
+    </Form>
+    <div>
+        <Button type='primary' style={{ width: 100, height: 50, float: 'right' }} onClick={() => { setIsSign(false) }}>
+            Sign Up
         </Button>
     </div>
+    </Card>
+    : <Card style={{ width: 500, height: 350 }}>
+    <strong style={{ fontSize: 20 }}>
+     儿童医院预约挂号系统
+    </strong>
+    <Form style={{ marginTop: 50, marginBottom: 50 }} onFinish={() => {}}>
+        <Item label='User' style={{ height: 30 }}>
+           <Input></Input>
+        </Item>
+        <Item label='Password'>
+            <Input></Input>
+        </Item>
+    </Form>
+    <Button type='primary' style={{ width: 100, marginRight: 100, height: 50 }} onClick={() => { setIsSign(true) }}>
+        Sign Up
+    </Button>
+    <Button type='primary' style={{ width: 100, height: 50 }} onClick={() => { alert(2) }}>
+        Login
+    </Button>
+    </Card>
 }
-
 export default Login
